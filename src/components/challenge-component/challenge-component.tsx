16bded2291c3e4a1062cce7245a14ce6a5416c45
Azra Efendic <azra_efendic@live.com>
2021-02-23 18:31:29 +0100

    feat: created component structure and initial style

--- src/components/challenge-component/challenge-component.tsx
+++ src/components/challenge-component/challenge-component.tsx
@@ -1,17 +1,32 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, h } from '@stencil/core';
 
 @Component({
   tag: 'challenge-component',
   styleUrl: 'challenge-component.scss',
   shadow: true,
+  // assetsDirs: ['challenge-assets'],
 })
 export class ChallengeComponent {
-  // eslint-disable-next-line class-methods-use-this
-  render() {
+// eslint-disable-next-line class-methods-use-this
+  public render(): JSX.Element {
     return (
-      <Host>
-        <slot></slot>
-      </Host>
+      <div class='component'>
+        <header>CtA - Main conversion</header>
+        <div class='container'>
+          <div class='content'>
+            <div>
+              <h3>A wonderful serenity has taken possession</h3>
+              <p>
+                A wonderful serenity has taken possession of my entire soul, like these sweet...
+              </p>
+              <button>Link button</button>
+            </div>
+          </div>
+          <div class='image'>
+            <img src='https://c4.wallpaperflare.com/wallpaper/588/367/704/nature-water-mountains-landscape-wallpaper-preview.jpg'/>
+          </div>
+        </div>
+      </div>
     );
   }
 }
